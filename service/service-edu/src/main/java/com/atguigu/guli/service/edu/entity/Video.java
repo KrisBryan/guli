02fdcad031d6efb36ffedd24b4420eb63a39a043
kris <jiegeailaopo@qq.com>
2021-03-03 19:53:00 +0800

    swagger使用

--- service/service-edu/src/main/java/com/atguigu/guli/service/edu/entity/Video.java
+++ service/service-edu/src/main/java/com/atguigu/guli/service/edu/entity/Video.java
@@ -1,5 +1,6 @@
 package com.atguigu.guli.service.edu.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.atguigu.guli.service.base.model.BaseEntity;
@@ -9,6 +10,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import com.baomidou.mybatisplus.annotation.TableField;
 
 /**
  * <p>
